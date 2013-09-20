ff745554727f8996cf27761d4f7d65763662891d
Matt Stanford <mds6058@yahoo.com>
2013-09-20 12:25:39 -0400

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackernewsParse::Application.config.secret_key_base = 'abdd6e201c04752facfabdf976c2aeb8a4a5bb0b9edcbb858d0bf6fb1ee8cdd0ab6b47e3ee4f526ebf65be4873fe7375264cf27ab64e8807051089d6a5a65878'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackernewsParse::Application.config.session_store :cookie_store, key: '_hackernews_parse_session'
